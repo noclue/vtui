4deee857599c13fe83b89746708c5c0a814d96e0
Kiril Kartaatanasov <karaatanasov@gmail.com>
2026-04-10 10:40:35 +0300

    fix(prop-browser): preserve unicode in pretty-printed JSON
    Fixed a regression that caused corruption of unicode data when saving JSON.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+* fix(prop-browser): preserve unicode in pretty-printed JSON
+
 ## [0.2.4] - 2026-04-05
 
 ### Added
--- src/prop_browser/prop_utils.rs
+++ src/prop_browser/prop_utils.rs
@@ -35,3 +35,56 @@ pub fn to_json_value(value: &VimAny, name: &str) -> anyhow::Result<Value> {
         }
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use vim_rs::types::boxed_types::ValueElements;
+    use vim_rs::types::structs::{VAppIpAssignmentInfo, VmConfigInfo};
+
+    use super::*;
+
+    #[test]
+    fn test_to_json_value() {
+        let value = VimAny::Value(ValueElements::PrimitiveString("test © “quoted”".to_string()));
+        let json_value = to_json_value(&value, "test").unwrap();
+        match json_value {
+            Value::String(s) => assert_eq!(s, "test © “quoted”"),
+            _ => panic!("Expected String"),
+        }
+    }
+    #[test]
+    fn test_to_json_value_object() {
+        let value = VimAny::Object(Box::new(VmConfigInfo {
+            product: None,
+            property: None,
+            ip_assignment: VAppIpAssignmentInfo { 
+                supported_allocation_scheme: None, 
+                ip_allocation_policy: None, 
+                supported_ip_protocol: None, 
+                ip_protocol: None 
+            },
+            eula: Some(Vec::from(["test © “quoted”".to_string()])),
+            ovf_section: None,
+            ovf_environment_transport: None,
+            install_boot_required: false,
+            install_boot_stop_delay: 0,
+        }));
+        let json_value = to_json_value(&value, "test").unwrap();
+        match json_value {
+            Value::Object(obj) => {
+                let eula = obj.get("eula").unwrap();
+                match eula {
+                    Value::Array(arr) => {
+                        assert_eq!(arr.len(), 1);
+                        match &arr[0] {
+                            Value::String(s) => assert_eq!(s, "test © “quoted”"),
+                            _ => panic!("Expected String"),
+                        }
+                    }
+                    _ => panic!("Expected Array"),
+                }
+            }
+            _ => panic!("Expected Object"),
+        };
+    }
+}
